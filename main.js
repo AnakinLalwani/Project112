0d6afc4a9330b8d8fc182a19c94d7113ea325952
AnakinLalwani <72091898+AnakinLalwani@users.noreply.github.com>
2021-04-17 22:12:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function tm() {
+    window.location.href = "https://anakinlalwani.github.io/Class103/";
+}
+function mn() {
+    window.location.href = "https://anakinlalwani.github.io/Class112/";
+}
+
+function testpage() {
+    window.location = "test.html"
+}
+
+function back() {
+    window.location = "index.html"
+}
